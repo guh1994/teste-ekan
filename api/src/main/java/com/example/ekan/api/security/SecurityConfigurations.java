c8af5bfebb76c42c2e4eef11044fa890752c906b
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2024-07-15 08:20:39 -0300

    FEAT: Access to use H2 on web

--- api/src/main/java/com/example/ekan/api/security/SecurityConfigurations.java
+++ api/src/main/java/com/example/ekan/api/security/SecurityConfigurations.java
@@ -30,7 +30,7 @@ public class SecurityConfigurations {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST,"/login").permitAll()
-                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/v3/api-docs/**","/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .requestMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated().and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
